04073c6153b328a7661303e2bb54f1d8f36265f7
rattling <curry.john@gmail.com>
2018-10-24 09:43:38 +0100

    First Working Version
    Calcs the RMSE correctly from logs.  Some nice bits around replacement of NaNs.

--- HousePricePred.py
+++ HousePricePred.py
@@ -1,46 +1,41 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr 22 17:43:56 2018
-
-
-"""
-#test
-
+@@ -8,10 +8,12 @@ Created on Sun Apr 22 17:43:56 2018
 
 import pandas as pd
 import os
+import numpy as np
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn import metrics
+import math 
 
 ################
 #IMPORT THE DATA
-################
-base_dir = 'C:/Users/John/Documents/GitHub/HousePrices/'
-input_dir = base_dir + 'input/'
-output_dir = base_dir + 'output/' 
-file_name='train.csv'
-abt= pd.read_csv(os.path.join(input_dir,file_name), encoding='utf8')
-
-#######################
-#CREATE DUMMY VARIABLES
+@ -27,7 +29,10 @@ abt= pd.read_csv(os.path.join(input_dir,file_name), encoding='utf8')
 #######################
 cat_list = list(abt.select_dtypes(include=['object']).columns)
 #abt2= pd.get_dummies(abt, columns=["Street", "LotConfig" ], prefix=["ST", "LC"])
 abt2= pd.get_dummies(abt, columns=cat_list, prefix=cat_list)
+abt2.isnull().sum(axis = 0).sort_values(ascending=False)
+abt2['LotFrontage'].fillna(0, inplace=True)
+abt2['GarageYrBlt'].fillna(abt2['GarageYrBlt'].mean(), inplace=True)
+abt2['MasVnrArea'].fillna(abt2['MasVnrArea'].mean(), inplace=True)
 
 
 train=abt2.sample(frac=0.7,random_state=200)
-test=abt2.drop(train.index)
+@ -35,20 +40,34 @@ test=abt2.drop(train.index)
 
 train_y = train.loc[:,['SalePrice']]
 train_x = train.drop('SalePrice', 1)
 
+test_y = test.loc[:,['SalePrice']]
+test_x = test.drop('SalePrice', 1)
 #########################
 #NEXT: DEAL WITH NULLS
 #########################
 abt.isnull().sum(axis = 0)
+a = abt2.isnull().sum(axis = 0)
 #list only cols with nulls
 #If over 1000 nulls just delete col
 #If categoricals have nulls what happens with the dummy encoding? Unknown?
@@ -51,5 +46,20 @@ abt.isnull().sum(axis = 0)
 
 # Create linear regression object
 regr = linear_model.LinearRegression()
+# Fit the regression
 regr.fit(train_x, train_y)
 
+#Make predictions on the test set
+pred_y = np.asarray(regr.predict(test_x))
+
+#Replace negative/zero values wtih 1 so can take log
+pred_y[pred_y<=0] = 1
+#Take log to calculate the competition metric RMSE of log values
+log_pred_y = np.log(pred_y)
+
+#Repeat for the test set
+test_y = np.asarray(test_y)
+log_test_y = np.log(test_y)
+
+#Calculate the RMSE
+rmse=np.sqrt(metrics.mean_squared_error(log_test_y, log_pred_y))
